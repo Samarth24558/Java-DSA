e022728bcd6cdbb86b25ec39c425548bdddbad18
Samarth <samarth24558@gmail.com>
2024-05-04 22:47:04 +0530

    added 2 files and changed file for 2 files

---
+++ Java programmes/for_each_loop.java
@@ -0,0 +1,15 @@
+public class for_each_loop 
+{
+    public static void main(String[] args) 
+    {
+        int[] arr={1,2,3,5,60,20};
+
+        for (int value: arr)
+        {
+            System.out.println(value);
+
+        }
+        
+    }
+    
+}
---
+++ Java programmes/string_manipulation.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class string_manipulation 
+{
+    public static void main(String[] args) 
+    {
+        // java program to perform various string manipulations
+
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter any word:");
+        String value=input.nextLine();
+        StringBuilder sb=new StringBuilder(value);
+        System.out.println("Length of string is: "+sb.length());
+        System.out.println("Append: "+sb.append(" Hello world"));
+        System.out.println("Insert: "+sb.insert(0,"S"));
+        System.out.println("Delete: "+sb.delete(1, 3));
+
+        
+    }
+
+    
+}
