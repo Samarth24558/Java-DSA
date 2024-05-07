caab776b0dd6c844d5a533c322b28421ade66485
Samarth <samarth24558@gmail.com>
2024-05-07 18:39:46 +0530

    Modified expresion_evaluation.java file

--- Java programmes/expresion_evaluation.java
+++ Java programmes/expresion_evaluation.java
@@ -6,8 +6,8 @@ public class expresion_evaluation
 
         Scanner input=new Scanner(System.in);
         System.out.print("Enter the radius of semi-circle: ");
-        double radius=input.nextDouble();
-        double area=1/2*(3.14*radius*radius);
+        float radius=input.nextFloat();
+        float area=(3.14f*radius*radius)/2;
         System.out.println("Area of semi-circle is: "+area);
 
         
